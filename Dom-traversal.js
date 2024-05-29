591904e36ab7720213bfa547c4c045b468d0b3b6
Jaskaran Singh <jaskaransingh4704@gmail.com>
2024-05-29 07:56:40 +0530

    Dom traversal

---
+++ Dom-traversal.js
@@ -0,0 +1,28 @@
+// Parent Node
+const parent = document.getElementById("parent");
+// console.log(parent);
+// console.log(parent.parentNode)
+
+
+// console.log(parent.childNodes)
+// console.log(parent.children);
+
+// console.log(parent.firstChild)
+// console.log(parent.firstElementChild)
+
+// console.log(parent.lastChild)
+// console.log(parent.lastElementChild)
+
+const child1 = document.getElementById("child1");
+// console.log(child1.nextSibling)
+// console.log(child1.nextElementSibling)
+
+const child2 = document.getElementById("child2");
+// console.log(child2.previousSibling)
+// console.log(child2.previousElementSibling)
+
+const list = document.getElementById("list");
+console.log(list.firstChild)
+console.log(list.firstElementChild)
+console.log(list.lastChild)
+console.log(list.lastElementChild)
